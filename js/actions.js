a09c345c7b20c6567d739a2515d46c3689f23e00
MarioRG <mario.rios.guzman@gmail.com>
2013-07-20 18:56:07 -0500

    ver 0.1a

---
+++ js/actions.js
@@ -0,0 +1,33 @@
+//actions
+var jQT = new $.jQTouch({
+	themeSelectionSelector: '#jqt'
+});
+
+$(document).ready(function(){       //puede uarse $(function(){
+    // Acelerometro
+    var watchID=null
+    $('#acelerometro .individual li').tap(function()
+    {
+      if($(this).index()==0){      //Boton iniciar
+          function onSuccess(acceleration) {
+              $('#acelerometro h2').html('Acceleration X: ' + acceleration.x + '<br>' +
+                    'Acceleration Y: ' + acceleration.y + '<br>' +
+                    'Acceleration Z: ' + acceleration.z + '<br>');
+          };
+
+          function onError() {
+              alert('onError!');
+          };
+
+var options = { frequency: 500 };  // Update every 3 seconds
+
+watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+      } else {                     //Boton Detener
+      if (watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+          $('#acelerometro h2').html('detenido');
+        }
+      }
+    });
+});
